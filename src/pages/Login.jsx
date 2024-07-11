50f90c48433f016eb5958a637662fc490e4b4c2f
Ram742k <ramkumar742k@gmail.com>
2024-07-11 14:39:55 +0530

    add loader

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
 import { login } from '../services/userServices';
 import { useNavigate, Link } from 'react-router-dom';
-import { ThreeDots } from 'react-loader-spinner';
 
 const Login = () => {
   const [formData, setFormData] = useState({ email: '', password: '' });
   const [error, setError] = useState('');
-  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -15,7 +13,6 @@ const Login = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setLoading(true);
     try {
       const response = await login(formData);
       const token = response.data.token;
@@ -24,22 +21,20 @@ const Login = () => {
       setError('');
     } catch (error) {
       setError('Invalid credentials. Please try again.');
-    } finally {
-      setLoading(false);
     }
   };
 
   return (
-    <div className='container'>
-      <div className='row d-flex justify-content-center align-items-center vh-80'>
+   <div className='container'>
+    <div className='row d-flex justify-content-center align-items-center vh-80'>
         <div className='card col-12 col-md-6 col-lg-4'>
-          <div>
-            <h2 className='mb-5'>Login</h2>
-            <form onSubmit={handleSubmit}>
-              <div className='row form-group mt-4'>
-                <label className='col-4' htmlFor='email'>Username</label>
-                <div className='col-8'>
-                  <input
+        <div>
+      <h2 className='mb-5'>Login</h2>
+      <form onSubmit={handleSubmit}>
+       <div className='row form-group mt-4'>
+            <label className='col-4' htmlFor='email'>Username</label>
+            <div className='col-8'>
+                <input
                     type="email"
                     value={formData.email}
                     name="email"
@@ -47,13 +42,13 @@ const Login = () => {
                     onChange={handleChange}
                     placeholder="Email"
                     required
-                  />
-                </div>
-              </div>
-              <div className='row form-group mt-4'>
-                <label className='col-4' htmlFor='password'>Password</label>
-                <div className='col-8'>
-                  <input
+                />
+            </div>
+       </div>
+         <div className='row form-group mt-4'>
+            <label className='col-4' htmlFor='password'>Password</label>
+            <div className='col-8'>
+                 <input
                     type="password"
                     name="password"
                     className='form-control'
@@ -61,29 +56,20 @@ const Login = () => {
                     onChange={handleChange}
                     placeholder="Password"
                     required
-                  />
-                </div>
-              </div>
-              
-              <button type="submit" className='btn btn-primary mt-5 w-100' disabled={loading}>
-                {loading ? (
-                  <ThreeDots 
-                    type="ThreeDots" 
-                    color="#00BFFF" 
-                    height={20} 
-                    width={20} 
-                  />
-                ) : 'Login'}
-              </button>
-            </form>
+                    />
+            </div>
+         </div>
+        
+        <button type="submit" className='btn btn-primary mt-5 w-100'>Login</button>
+      </form>
 
-            <p className='mt-5'>Don't have an account? <Link to="/register">Register</Link></p>
-            <p className='mt-3'>Forgot your password? <Link to="/forgot-password">Reset Password</Link></p>
-            {error && <div style={{ color: 'red' }}>{error}</div>}
-          </div>  
+        <p className='mt-5'>Don't have an account? <Link to="/register">Register</Link></p>
+        <p className='mt-3'>Forgot your password? <Link to="/forgot-password">Reset Password</Link></p>
+      {error && <div style={{ color: 'red' }}>{error}</div>}
+    </div>  
         </div>
-      </div>
     </div>
+   </div>
   );
 };
 
